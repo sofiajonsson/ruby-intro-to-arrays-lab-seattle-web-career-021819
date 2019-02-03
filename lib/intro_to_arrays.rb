ee82affa3c9d0e8eddd3eb0485990e144590e5fa
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 20:48:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,12 +10,12 @@ def first_element(my_first_element)
  my_first_element[0]
 end
 
-def third_element
+def third_element(my_third_element)
   my_third_element[2]
 end
 
-def last_element
-  my_last_element.last
+def last_element(my_last_element)
+  my_last_element[-1]
 end
 
 
