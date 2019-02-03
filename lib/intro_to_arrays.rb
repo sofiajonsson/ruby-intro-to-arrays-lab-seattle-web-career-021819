d92855447b173134fe7c404369d390a9d73f0e59
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 20:31:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,12 +1,11 @@
 def my_new_array
- instantiate_new_array = []
- instantiate_new_array.inspect
+
 end
 
 def array_with_two_elements
-  array = ["hello", "goodbye" ]
-  array.inspect
-end
+#   array = ["hello", "goodbye" ]
+#   array.inspect
+# end
 
 def
 end
