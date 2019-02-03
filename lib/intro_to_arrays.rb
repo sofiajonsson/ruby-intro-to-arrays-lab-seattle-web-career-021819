14215db04b94e30524f4dddb08ec94053bb2db0e
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 20:54:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,10 +22,13 @@ def first_element_with_array_methods(first_country)
   first_country.first 
 end 
 
-def last_element_with_array_methods
+def last_element_with_array_methods(last_country)
+  last_country.last
 end
 
-def \""
+def length_of_array(length)
+  length.count 
+end 
   
   
   
