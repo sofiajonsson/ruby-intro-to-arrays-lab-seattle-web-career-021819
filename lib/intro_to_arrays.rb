beb7baf6d7bcbc7ca7a0fbafe35832c5743d5bcc
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 20:54:13 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,44 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+my_two_array = [1, 2]
+end
+
+def first_element(my_first_element)
+ my_first_element[0]
+end
+
+def third_element(my_third_element)
+  my_third_element[2]
+end
+
+def last_element(my_last_element)
+  my_last_element[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  first_country.first 
+end 
+
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def length_of_array(length)
+  length.count 
+end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
